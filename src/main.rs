2480696c27cb8466bb0b07239e65564a7f1fad0d
Christoph Heiss <me@christoph-heiss.me>
2021-04-23 23:33:36 +0200

    deps: Update `cursive` and `cursive_table_view`.
    Now that `cursive_table_view` was updated to support the newest version of
    `cursive`, we can _finally_ get rid of the git version of `cursive_table_view`
    and use the crates.io one.

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,8 @@
 mod login;
 mod vault;
 
-use cursive::backends::termion::Backend;
-use cursive::Cursive;
-use cursive_buffered_backend::BufferedBackend;
-
 fn main() {
-    // We need to use a buffered backend due to flickering with termion.
-    let mut siv = Cursive::new(|| {
-        let backend = Backend::init().unwrap();
-        let buffered = BufferedBackend::new(backend);
-
-        Box::new(buffered)
-    });
+    let mut siv = cursive::default();
 
     if let Ok(vault) = vault::read_local_data() {
         siv.set_user_data(vault);
